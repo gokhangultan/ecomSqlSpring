7275763ed063361c9a8548fcebda4987600b1cfa
gokhangultan <gultangokhan@gmail.com>
2024-05-12 12:19:10 +0300

    seucirty layer main

--- src/main/java/com/ecom/gg/ecomgg/config/SecurityConfig.java
+++ src/main/java/com/ecom/gg/ecomgg/config/SecurityConfig.java
@@ -2,13 +2,25 @@ package com.ecom.gg.ecomgg.config;
 
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+
+//csrf --> cross site resource forgery
 @Configurable
 public class SecurityConfig {
+
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
 
+        return httpSecurity.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth->{
+                    auth.anyRequest().permitAll();
+                })
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .build();
     }
 }
