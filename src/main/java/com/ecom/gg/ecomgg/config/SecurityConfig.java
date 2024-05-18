ec25713a233fbd46f9773447a25a73c1c6c16948
gokhangultan <gultangokhan@gmail.com>
2024-05-18 23:12:05 +0300

    CORS configuration Source

--- src/main/java/com/ecom/gg/ecomgg/config/SecurityConfig.java
+++ src/main/java/com/ecom/gg/ecomgg/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.ecom.gg.ecomgg.config;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -12,6 +13,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 
 //csrf --> cross site resource forgery
@@ -32,10 +38,22 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173","https://ecomapi.gokhangultan.com"));
+        corsConfiguration.setAllowedMethods(List.of("GET","PUT","DELETE","POST"));
+        corsConfiguration.setAllowedHeaders(List.of(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",corsConfiguration);
+    return source;
+
+
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
-
+    httpSecurity.cors().configurationSource(corsConfigurationSource());
         return httpSecurity.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth->{
                     auth.requestMatchers("/products/**").hasAuthority("ADMIN");
